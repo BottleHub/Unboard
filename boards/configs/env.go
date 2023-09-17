02825324819919531319e02604ab4c8426e6b864
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-09-17 23:57:50 +0100

    Quick Fixes

--- boards/configs/env.go
+++ boards/configs/env.go
@@ -20,11 +20,14 @@ func EnvMongoURI() string {
 }
 
 func EnvRabbitMQ() string {
-	err := godotenv.Load()
-	if err != nil {
-		err := godotenv.Load(".env.test")
+	_, err := os.LookupEnv("RABBITMQ")
+	if !err {
+		err := godotenv.Load()
 		if err != nil {
-			log.Fatal("Error loading .env file: ", err)
+			err := godotenv.Load(".env.test")
+			if err != nil {
+				log.Fatal("Error loading .env file: ", err)
+			}
 		}
 	}
 	return os.Getenv("RABBITMQ")
