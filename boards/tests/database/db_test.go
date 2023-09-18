78a1de47f1110d523d9f53ea30af1a5087903268
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-09-18 17:35:32 +0100

    Fixes

--- boards/tests/database/db_test.go
+++ boards/tests/database/db_test.go
@@ -1,7 +1,13 @@
 package database_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/bottlehub/unboard/boards/internals/database"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestConnectDB(t *testing.T) {
-
+	_, err := database.ConnectDB()
+	assert.NoError(t, err)
 }
