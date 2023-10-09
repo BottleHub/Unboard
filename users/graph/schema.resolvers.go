60c21848adf8ce3f9d0dc9ac12557afa22647efd
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-10-09 11:39:25 +0100

    Update

--- users/graph/schema.resolvers.go
+++ users/graph/schema.resolvers.go
@@ -71,9 +71,9 @@ func (r *queryResolver) User(ctx context.Context, input model.Fetch) (*model.Use
 	return user, err
 }
 
-// Links is the resolver for the links field.
-func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
-	panic(fmt.Errorf("not implemented: Links - links"))
+// Link is the resolver for the link field.
+func (r *queryResolver) Link(ctx context.Context) (*model.Link, error) {
+	panic(fmt.Errorf("not implemented: Link - link"))
 }
 
 // Mutation returns MutationResolver implementation.
@@ -84,6 +84,16 @@ func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//     it when you're done.
+//   - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
+	panic(fmt.Errorf("not implemented: Links - links"))
+}
 
 var (
 	db, _ = database.ConnectDB()
